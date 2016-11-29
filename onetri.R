850a3760748d5e0b32d2298c62b34aead9f00296
Mike Wise <mwise@microsoft.com>
2016-11-29 13:13:28 +0100

    compressed vertices - optimized various things

---
+++ onetri.R
@@ -0,0 +1,34 @@
+library(rgl)
+open3d()
+
+addVert <- function(mList,v) {
+  ni <- length(mList$idxList)
+  np <- length(mList$pntList)
+  #  mList$pntList[[ np+1 ]] <- v[1]
+  #  mList$pntList[[ np+2 ]] <- v[2]
+  #  mList$pntList[[ np+3 ]] <- v[3]
+  #  mList$idxList[[ ni+1 ]] <- ni+1
+  mList$pntList <- c(mList$pntList,v[1],v[2],v[3])
+  mList$idxList <- c(mList$idxList,ni + 1)
+  return(mList)
+}
+
+addTri <- function(mList,v1,v2,v3) {
+  mList <- addVert(mList,v1)
+  mList <- addVert(mList,v2)
+  mList <- addVert(mList,v3)
+  return(mList)
+}
+
+mList <- list()
+mList$idxList <- list()
+mList$pntList <- list()
+
+v1 <- c(1,0,0)
+v2 <- c(0,1,0)
+v3 <- c(0,0,1)
+mList <- addTri(mList,v1,v2,v3)
+vidx <- unlist(mList$idxList)
+vpnt <- unlist(mList$pntList)
+part <- tmesh3d(vpnt,vidx,homogeneous = F)
+wire3d(part)
--- rgl4crazyflie.R
+++ rgl4crazyflie.R
@@ -1,14 +1,21 @@
 library(rgl)
 library(stringr)
 library(xml2)
+library(hash)
+
+starttime <- Sys.time()
 
 stldir <- "../Crazyflie-CAD/STL"
 
 stlfiles <- list.files(stldir,"\\.STL$")
 
-append <- function(vlist, v) {
+appendWithAtts <- function(vlist,v) {
+  # Append to a list and retain it attributes
   if (!is.null(v)) {
     vlist[[length(vlist) + 1]] <- v
+    # Note, if we use the faster method below, we loose the attributes of vlist!
+    # Bad, bad, bad...
+    # vlist <- c(vlist,v)
   }
   return(vlist)
 }
@@ -54,26 +61,87 @@ loadStl <- function(stldir, stlfname) {
   for (line in lines) {
     if (str_detect(line, "facet")) {
       if (length(facet) == 4) {
-        partlist <- append(partlist, facet)
+        partlist <- appendWithAtts(partlist, facet)
       }
       facet <- list()
-      facet <- append(facet, extractVert(line))
+      facet <- appendWithAtts(facet, extractVert(line))
     } else if (str_detect(line, "vertex")) {
-      facet <- append(facet, extractVert(line))
+      facet <- appendWithAtts(facet, extractVert(line))
     } else {
       print(sprintf("opps unknown linetype:%s",line))
     }
   }
-  partlist <- append(partlist, facet)
+  partlist <- appendWithAtts(partlist, facet)
   elap <- as.numeric((Sys.time() - starttime)[1], units = "secs")
   print(sprintf("Extracted %d vertices in %.1f secs for part '%s'", length(partlist),elap,partname))
   return(partlist)
 }
 
+# hash bookkeeping
+vhashtab <- hash()
+nvsaved <- 0
+nvtot <- 0
 
+addVertHashed <- function(mList,v) {
+  ni <- length(mList$idxList)
+  np <- length(mList$pntList)
+  vkey <- paste0(sprintf("%e",v),collapse=",")
+  if (has.key(vkey,vhashtab)) {
+    nval <- vhashtab[[vkey]]
+    #print(sprintf("np:%d ni:%d vkey:%s nval:%d",np,ni,vkey,nval))
+    mList$idxList <- c(mList$idxList,nval)
+    nvsaved <<- nvsaved+1
+  } else {
+    vnum <- (np+3)/3
+    vhashtab[vkey] <- vnum
+    mList$pntList <- c(mList$pntList,v[1],v[2],v[3])
+    mList$idxList <- c(mList$idxList,vnum)
+    nvtot <<- nvtot+1
+  }
+  return(mList)
+}
 
+addVert <- function(mList,v) {
+  ni <- length(mList$idxList)
+  np <- length(mList$pntList)
+  mList$pntList <- c(mList$pntList, v[1],v[2],v[3])
+  mList$idxList <- c(mList$idxList, ni+1)
+  nvtot <<- nvtot+1
+  return(mList)
+}
 
-plotPart <- function(vertList, trn = c(0,0,0), rot=NULL, color="silver",alpha=1,shiny=shiny) {
+addTri <- function(mList, v1, v2, v3) {
+  mList <- addVertHashed(mList,v1)
+  mList <- addVertHashed(mList,v2)
+  mList <- addVertHashed(mList,v3)
+  return(mList)
+}
+
+plotPartAsMesh <- function(vertList,trn = c(0,0,0),rot = NULL,color = "silver",alpha = 1,shiny = shiny) {
+  vhashtab <<- hash()
+  mList <- list()
+  mList$idxList <- list()
+  mList$pntList <- list()
+
+  for (v in vertList) {
+    if (length(v) == 4) {
+      mList <- addTri(mList,v[[2]],v[[3]],v[[4]]) 
+    }
+  }
+  vidx <- unlist(mList$idxList)
+  vpnt <- unlist(mList$pntList)
+
+  nv <- length(vertList)
+  ni <- length(vidx)
+  np <- length(vpnt)
+  vsv <- 3*nv - np
+  print(sprintf("tmesh3d - nv:%d np:%d ni:%d maxi:%d mini:%d - vsaved:%d",nv,np,ni,max(vidx),min(vidx),vsv))
+  part <- tmesh3d(vpnt,vidx,homogeneous=F)
+  part <- translate3d(rotate3d(part,matrix=rot), trn[[1]],trn[[2]],trn[[3]] )
+  shade3d(part,color = color,alpha = alpha,shiny = shiny)
+}
+
+plotPart <- function(vertList,trn = c(0,0,0),rot = NULL,color = "silver",alpha = 1,shiny = shiny) {
   lx <- list()
   ly <- list()
   lz <- list()
@@ -82,24 +150,14 @@ plotPart <- function(vertList, trn = c(0,0,0), rot=NULL, color="silver",alpha=1,
   zi <- 3
   for (v in vertList) {
     if (length(v) == 4) {
-      lx <- append(lx, v[[2]][xi] )
-      lx <- append(lx, v[[3]][xi] )
-      lx <- append(lx, v[[4]][xi])
-
-      ly <- append(ly, v[[2]][yi] )
-      ly <- append(ly, v[[3]][yi] )
-      ly <- append(ly, v[[4]][yi])
-
-      lz <- append(lz, v[[2]][zi] )
-      lz <- append(lz, v[[3]][zi] )
-      lz <- append(lz, v[[4]][zi] )
+      lx <- c(lx,v[[2]][xi],v[[3]][xi],v[[4]][xi])
+      ly <- c(ly,v[[2]][yi],v[[3]][yi],v[[4]][yi])
+      lz <- c(lz,v[[2]][zi],v[[3]][zi],v[[4]][zi])
     }
   }
-  vx <- unlist(lx) 
-  vy <- unlist(ly) 
-  vz <- unlist(lz) 
-
-
+  vx <- unlist(lx)
+  vy <- unlist(ly)
+  vz <- unlist(lz)
 
   if (!is.null(rot)) {
     #print("rotating")
@@ -109,19 +167,19 @@ plotPart <- function(vertList, trn = c(0,0,0), rot=NULL, color="silver",alpha=1,
     vx <- ux*rot[1,1] + uy*rot[2,1] + uz*rot[3,1]
     vy <- ux*rot[1,2] + uy*rot[2,2] + uz*rot[3,2]
     vz <- ux*rot[1,3] + uy*rot[2,3] + uz*rot[3,3]
-#    vx <- ux*rot[1,1] + uy*rot[1,2] + uz*rot[1,3]
-#    vy <- ux*rot[2,1] + uy*rot[2,2] + uz*rot[2,3]
-#    vz <- ux*rot[3,1] + uy*rot[3,2] + uz*rot[3,3]
   }
   #print("translating")
   vx <- vx + trn[[xi]]
   vy <- vy + trn[[yi]]
   vz <- vz + trn[[zi]]
 
-  print(sprintf("trn-q: %.3f %.3f %.3f ", trn[[xi]], trn[[yi]], trn[[zi]]))
+  #print(sprintf("trn-q: %.3f %.3f %.3f ",trn[[xi]],trn[[yi]],trn[[zi]]))
 
-  triangles3d( vx,vy,vz, color=color,alpha=alpha,shiny=shiny)
+  nvtot <<- nvtot + length(vx)
+
+  triangles3d(vx,vy,vz,color = color,alpha = alpha,shiny = shiny)
 }
+
 
 addAxes <- function(len = 1) {
   u <- c(0,1)*len
@@ -163,6 +221,7 @@ plotPartsInCircle <- function(partList, radius = 80) {
     alf <- attr(prt, "alpha")
     shn <- attr(prt, "shine")
     plotPart(prt, trn, NULL, clr,alf,shn)
+    #plotPartAsMesh(prt, trn, NULL, clr,alf,shn)
   }
   addAxes(len = rad/2)
 }
@@ -183,29 +242,6 @@ getElement <- function(partlist, need) {
   return(NULL)
 }
 
-#readxml <- F
-#if (readxml) {
-  #xmlfile <- sprintf("%s/%s", stldir, "Crazyflie_assembly.xml")
-  #doc <- read_xml(xmlfile)
-  #itree <- xml_find_first(doc, "//*[local-name()='InstanceTree']")
-  #its <- xml_find_all(doc, "//*[local-name()='Instance']")
-  #for (it in its) {
-    ##print(as.character(it))
-    #print(xml_attr(it, "name"))
-    #tform <- xml_find_first(it, "//*[local-name()='Transform']")
-    #if (length(tform) > 0) {
-      #rot <- xml_find_first(it, "//*[local-name()='Rotation']")
-      #print(sprintf("   %s", as.character(rot)))
-      #print(xml_text(rot))
-      #trn <- xml_find_first(it, "//*[local-name()='Translation']")
-      ##print(sprintf("   %s", as.character(trn)))
-      #print(xml_text(trn))
-    #}
-  #}
-#}
-
-
-
 plotWholeThing <- function(partList,doc) {
   itree <- xml_find_first(doc, "//*[local-name()='InstanceTree']")
   its <- xml_find_all(doc, "//*[local-name()='Instance']")
@@ -217,26 +253,20 @@ plotWholeThing <- function(partList,doc) {
     tform <- xml_find_first(it, ".//*[local-name()='Transform']")
     if (length(tform) > 0) {
       nrot <- xml_find_first(it, ".//*[local-name()='Rotation']")
-      #print(sprintf("   %s", as.character(nrot)))
-      print(xml_text(nrot))
       rot <- matrix(as.numeric(str_split(xml_text(nrot), "\\s")[[1]]),3,3)
-      print(rot)
 
-      ntrn <- xml_find_first(it, ".//*[local-name()='Translation']")
-      #print(sprintf("   %s", as.character(trn)))
-      print(xml_text(ntrn))
-      # no idea where this factor of 1000 comes from
+      ntrn <- xml_find_first(it,".//*[local-name()='Translation']")
+
+      # no idea where this factor of 1000 comes from (mm -> meters?)
       # some STL brain damage no doubt
-      trn <- 1000*as.numeric(str_split(xml_text(ntrn), "\\s")[[1]])
-
-      print(trn)
+      trn <- 1000 * as.numeric(str_split(xml_text(ntrn),"\\s")[[1]])
 
       prt <- getElement(partList,needpart)
       clr <- attr(prt, "color")
       alf <- attr(prt, "alpha")
       shn <- attr(prt, "shine")
-      plotPart(prt, trn, rot, clr, alf, shn)
-      print(" ")
+      #plotPart(prt, trn, rot, clr, alf, shn)
+      plotPartAsMesh(prt, trn, rot, clr, alf, shn)
 
     }
   }
@@ -249,3 +279,5 @@ doc <- read_xml(xmlfile)
 plotWholeThing(partList,doc)
 #d <- read_xml("simple1.xml")
 #bstree <- xml_find_all(d, ".//bs:stmt") # Transaction Type
+elap <- as.numeric((Sys.time() - starttime)[1], units = "secs")
+print(sprintf("Run took %.1f secs for %d verts - saved:%d", elap,nvtot,nvsaved))
