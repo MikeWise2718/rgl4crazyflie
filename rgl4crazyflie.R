7f7343aed3080555de65762721898127d24d6d25
Mike Wise <mwise@microsoft.com>
2016-12-01 12:06:58 +0100

    Extensive refactoring

--- rgl4crazyflie.R
+++ rgl4crazyflie.R
@@ -2,15 +2,24 @@ library(rgl)
 library(stringr)
 library(xml2)
 library(hash)
+#
+#  Mike Wise - Nov/Dec 2016
+#
+#  Program in R to plot CAD model of CrazyFlie mini-drone
+#
+#  It performs the following septs
+#   1. read in STL files from local copy of github repository Crazyflie-CAD
+#   2. create optimized triangle meshes out of it (optimized in reducing the number of points)
+#   3. orienting the data so it has the right x,y,z orientation
+#   4. ploting in in rgl
+#   5. writing out the data in a small set of csv files
+#  
 
-starttime <- Sys.time()
 
-stldir <- "../Crazyflie-CAD/STL"
-
-stlfiles <- list.files(stldir,"\\.STL$")
-
-appendWithAtts <- function(vlist,v) {
-  # Append to a list and retain it attributes
+appendListAsSubEl <- function(vlist,v) {
+  # Append a list to a list as a sub-element 
+  # i.e. do not merge the lists (thus losing structure)
+  # and retain it attributes 
   if (!is.null(v)) {
     vlist[[length(vlist) + 1]] <- v
     # Note, if we use the faster method below, we loose the attributes of vlist!
@@ -30,7 +39,7 @@ extractVert <- function(line) {
   return(rv)
 }
 
-loadStl <- function(stldir, stlfname) {
+readVertsFromStl <- function(stldir, stlfname,vertPartList) {
   starttime <- Sys.time()
   if (!is.null(stldir) & stldir!="") {
     stlfname <- sprintf("%s/%s", stldir, stlfname)
@@ -42,11 +51,13 @@ loadStl <- function(stldir, stlfname) {
     return(NULL)
   }
   print(sprintf("Read %d lines from %s",nlines0,stlfname))
-  partlist <- list()
+  vertList <- list()
 
   # extract the first line which is the name of the part
   partname <- lines[[1]]
-  attr(partlist, "partname") <- partname
+  partname <- gsub("solid ","",partname)
+  partname <- gsub("_Default_sldprt","",partname)
+
   lines <- lines[2:length(lines)]
 
   # trim out stuff we do not need
@@ -56,25 +67,27 @@ loadStl <- function(stldir, stlfname) {
   lines <- lines[!str_detect(lines, "endsolid")]
   print(sprintf("Stripped out %d irrelevant lines", nlines0-length(lines)))
 
-  # now loop over what is left and 
-  facet <- list()
+  # now loop over what is left and accumulate the facet vector
+  facetvek <- list()
   for (line in lines) {
     if (str_detect(line, "facet")) {
-      if (length(facet) == 4) {
-        partlist <- appendWithAtts(partlist, facet)
+      if (length(facetvek) == 4) {
+        vertList <- appendListAsSubEl(vertList,facetvek)
       }
-      facet <- list()
-      facet <- appendWithAtts(facet, extractVert(line))
+      facetvek <- list()
+      facetvek <- appendListAsSubEl(facetvek,extractVert(line))
     } else if (str_detect(line, "vertex")) {
-      facet <- appendWithAtts(facet, extractVert(line))
+      facetvek <- appendListAsSubEl(facetvek,extractVert(line))
     } else {
       print(sprintf("opps unknown linetype:%s",line))
     }
   }
-  partlist <- appendWithAtts(partlist, facet)
+  vertList <- appendListAsSubEl(vertList,facetvek) # add the remaining one tp vertlist
+
+  vertPartList[[partname]] <- vertList
   elap <- as.numeric((Sys.time() - starttime)[1], units = "secs")
-  print(sprintf("Extracted %d vertices in %.1f secs for part '%s'", length(partlist),elap,partname))
-  return(partlist)
+  print(sprintf("Extracted %d vertices in %.1f secs for part '%s'", length(vertList),elap,partname))
+  return(vertPartList)
 }
 
 # hash bookkeeping
@@ -117,10 +130,16 @@ addVert <- function(mList,v,n = NULL) {
   return(mList)
 }
 
-addTri <- function(mList, v1, v2, v3,norm) {
-  mList <- addVertHashed(mList,v1,norm)
-  mList <- addVertHashed(mList,v2,norm)
-  mList <- addVertHashed(mList,v3,norm)
+addTri <- function(mList,v1,v2,v3,norm,hashemup=T) {
+  if (hashemup) {
+    mList <- addVertHashed(mList,v1,norm)
+    mList <- addVertHashed(mList,v2,norm)
+    mList <- addVertHashed(mList,v3,norm)
+  } else {
+    mList <- addVert(mList,v1,norm)
+    mList <- addVert(mList,v2,norm)
+    mList <- addVert(mList,v3,norm)
+  }
   return(mList)
 }
 
@@ -132,26 +151,29 @@ normalize <- function(n) {
   return(n)
 }
 
-plotPartAsMesh <- function(compname,partname,vertList,trn = c(0,0,0),rot = NULL,
-                           color = "silver",alpha = 1,shiny = shiny,donorms = F,savefiles=F) {
+plotPartAsMesh <- function(compname,partname,vertPtList,trn = c(0,0,0),rot = NULL,
+                           color = "silver",alpha = 1,shiny = 50,donorms = F,hashemup=T) {
   vhashtab <<- hash()
   mList <- list()
   mList$idxList <- list()
   mList$pntList <- list()
   mList$nrmList <- list()
   normval <- NULL
-  for (v in vertList) {
+  for (v in vertPtList) {
     if (length(v) == 4) {
       if (donorms) normval <- v[[1]]
-      mList <- addTri(mList,v[[2]],v[[3]],v[[4]],normval) 
+      mList <- addTri(mList,v[[2]],v[[3]],v[[4]],normval,hashemup=hashemup) 
     }
   }
   vidx <- unlist(mList$idxList)
   vpnt <- unlist(mList$pntList)
-  nv <- length(vertList)
+  nv <- length(vertPtList)
   ni <- length(vidx)
   np <- length(vpnt)
 
+  # Not sure I can make donorms work right
+  # giving up for now - 2016.12.1
+  #
   if (donorms) {
     nn <- np / 3
     for (i in 1:nn) {
@@ -167,19 +189,22 @@ plotPartAsMesh <- function(compname,partname,vertList,trn = c(0,0,0),rot = NULL,
     vnrm <- unlist(mList$nrmList)
   }
 
-
-  vsv <- 3*nv - np
-  print(sprintf("   tmesh3d - nv:%d np:%d ni:%d maxi:%d mini:%d - vsaved:%d",nv,np,ni,max(vidx),min(vidx),vsv))
+  nvsv <- 3*nv - np
+  print(sprintf("   tmesh3d - nv:%d np:%d ni:%d maxi:%d mini:%d - vsaved:%d",nv,np,ni,max(vidx),min(vidx),nvsv))
   if (donorms) {
     part <- tmesh3d(vpnt,vidx,homogeneous=F, normals=vnrm)
   } else {
     part <- tmesh3d(vpnt,vidx,homogeneous=F)
   }
   part <- translate3d(rotate3d(part,matrix = rot),trn[[1]],trn[[2]],trn[[3]])
+
+  # Center and orient the drone - determined these from the data
+  # Drone was upside down and facing the wrong way
+  # making the center to be the center of the pcb board
+  #
   pcbcen <- -0.5*c(54.09200, 71.18820, 75.60720)
-  #pcbcen <- -10 * c(3.686,3.686,3.685)
   part <- translate3d(part,pcbcen[[1]],pcbcen[[2]],pcbcen[[3]])
-  part <- rotate3d(part,pi,0,1,0 )
+  part <- rotate3d(part,pi,0,1,0 ) # rotate 180 around the y-axis
   calccog <- T
   if (calccog) {
     vb <- part$vb
@@ -197,29 +222,27 @@ plotPartAsMesh <- function(compname,partname,vertList,trn = c(0,0,0),rot = NULL,
     ty <- max(yc) - min(yc)
     tz <- max(zc) - min(zc)
     tw <- max(wc) - min(wc)
-#    cx <- mean(vpnt[bsq+1])
-#    cy <- mean(vpnt[bsq+2])
-#    cz <- mean(vpnt[bsq+3])
+
     print(sprintf("   cog:%.5f %.5f %.5f %.5f thickness: %.5f %.5f %.5f %.5f",cx,cy,cz,cw, tx,ty,tz,tw))
   }
 
   shade3d(part,color = color,alpha = alpha,shiny = shiny)
-  if (savefiles) {
-  }
 }
 
-plotPart <- function(compname,partname,vertList,trn = c(0,0,0),rot = NULL,color = "silver",alpha = 1,shiny = shiny) {
+plotPart <- function(compname,partname,vertPtList,trn = c(0,0,0),
+                        rot = NULL,color = "silver",alpha = 1,shiny = 50) {
+# This was just a dev routine using triangles3d
+# it is a lot faster since we don't have to hash the vertices for optimization
+# but it has a lot more points (120k instead of around 22k)
   lx <- list()
   ly <- list()
   lz <- list()
-  xi <- 1
-  yi <- 2
-  zi <- 3
-  for (v in vertList) {
+
+  for (v in vertPtList) {
     if (length(v) == 4) {
-      lx <- c(lx,v[[2]][xi],v[[3]][xi],v[[4]][xi])
-      ly <- c(ly,v[[2]][yi],v[[3]][yi],v[[4]][yi])
-      lz <- c(lz,v[[2]][zi],v[[3]][zi],v[[4]][zi])
+      lx <- c(lx,v[[2]][1],v[[3]][1],v[[4]][1])
+      ly <- c(ly,v[[2]][2],v[[3]][2],v[[4]][2])
+      lz <- c(lz,v[[2]][3],v[[3]][3],v[[4]][3])
     }
   }
   vx <- unlist(lx)
@@ -236,17 +259,14 @@ plotPart <- function(compname,partname,vertList,trn = c(0,0,0),rot = NULL,color 
     vz <- ux*rot[1,3] + uy*rot[2,3] + uz*rot[3,3]
   }
   #print("translating")
-  vx <- vx + trn[[xi]]
-  vy <- vy + trn[[yi]]
-  vz <- vz + trn[[zi]]
-
-  #print(sprintf("trn-q: %.3f %.3f %.3f ",trn[[xi]],trn[[yi]],trn[[zi]]))
+  vx <- vx + trn[[1]]
+  vy <- vy + trn[[2]]
+  vz <- vz + trn[[3]]
 
   nvtot <<- nvtot + length(vx)
 
   triangles3d(vx,vy,vz,color = color,alpha = alpha,shiny = shiny)
 }
-
 
 addAxes <- function(len = 1) {
   u <- c(0,1)*len
@@ -260,85 +280,36 @@ addAxes <- function(len = 1) {
   text3d(v,w,u,c("","Z"),color=c("blue"))
 }
 
-
-# Load everything
-partList <- sapply(stlfiles, function(x) loadStl(stldir, x))
-
-# Temp and ugly
-colors <- c("white", "gray", "gray", "green", "yellow", "blue", "black", "orange")
-alpha <- c(0.4, 1, 1, 1, 1, 1, 1, 1)
-shine <- c(50, 50, 50, 50, 50, 50, 50, 50)
-for (i in 1:8) {
-  attr(partList[[i]],"color") <- colors[[i]]
-  attr(partList[[i]],"alpha") <- alpha[[i]]
-  attr(partList[[i]],"shine") <- shine[[i]]
-}
-
-plotPartsInCircle <- function(partList, radius = 80) {
-  n <- length(partList)
-  for (i in 1:n) {
-    prt <- partList[[i]]
-    print(attr(prt,"partname"))
-    rad <- radius
-    ang <- (i-1)*2*pi/n
-    cosang <- cos(ang)
-    sinang <- sin(ang)
-    trn <- c(rad * cosang, rad * sinang, 0)
-    clr <- attr(prt,"color")
-    alf <- attr(prt, "alpha")
-    shn <- attr(prt, "shine")
-    plotPart(prt, trn, NULL, clr,alf,shn)
-    #plotPartAsMesh(prt, trn, NULL, clr,alf,shn)
-  }
-  addAxes(len = rad/2)
-}
-
-#plotPartsInCircle(partList, 50)
-
-getElement <- function(partlist, need) {
-  #print(sprintf("getElement:%s",need))
-  for (p in partlist) {
-    pelid <- attr(p,"partname")
-    #print(sprintf("   detecting:%s", pelid))
-    if (str_detect(pelid,need)) {
-      #print(sprintf("returning:%s", pelid))
-      return(p)
-    }
-  }
-  #print(sprintf("returning NULL"))
-  return(NULL)
-}
-
-plotWholeThing <- function(partAttList,partList,compList) {
+plotWholeThing <- function(partAttList,partVertList,compList) {
   for (cp in compList) {
 
     compname <- cp$compname
-    partname <- str_split(compname,"-")[[1]][[1]]
+    partname <- gsub("\\-\\d$","",compname)
     print(sprintf("Comp:%s part:%s",compname,partname))
     rot <- cp$rot
     trn <- cp$trn
 
-    prt <- getElement(partList,partname)
-    clr <- attr(prt,"color")
-    alf <- attr(prt,"alpha")
-    shn <- attr(prt,"shine")
+    #vertPtList <- findVertPtList(partVertList,partname)
+    vertPtList <- partVertList[[partname]]
 
-    prta <- getAttElement(partAttList,partname)
+    prta <- partAttList[[partname]]
+    #if (is.null(prta)) { 
+        #prta <- findPartInPartAttList(partAttList,partname)
+    #}
+    #print(prta)
     iclr <- round(255 * prta$ambient[1:3])
     clr <- sprintf("#%2.2x%2.2x%2.2x",iclr[[1]],iclr[[2]],iclr[[3]])
-    #alf <- prta$ambient[4]
-    print(prta$ambient)
+    alf <- prta$ambient[4]
+    #print(prta$ambient)
 
-
-      #plotPart(compname,partname,prt, trn, rot, clr, alf, shn)
-    plotPartAsMesh(compname,partname,prt,trn,rot,clr,alf,shn)
+    #plotPart(compname,partname,vertPtList,trn,rot,clr,alf)
+    plotPartAsMesh(compname,partname,vertPtList,trn,rot,clr,alf,hashemup=T)
   }
   addAxes(len = 50)
   axes3d()
 }
 
-
-grabComposition <- function(stldir,xfname) {
+readCompositionFromXml <- function(stldir,xfname) {
   compList <- list()
   xmlfile <- sprintf("%s/%s",stldir,xfname)
   doc <- read_xml(xmlfile)
@@ -347,57 +318,29 @@ grabComposition <- function(stldir,xfname) {
   for (it in its) {
     #print(as.character(it))
     compname <- xml_attr(it, "name")
-    needpart <- str_split(compname,"-")[[1]][[1]]
-    thisCompPart <- list()
-    thisCompPart$compname <- compname
-    thisCompPart$partname <- needpart
+    needpart <- gsub("\\-\\d$","",compname)
+    cp <- list()
+    cp$compname <- compname
+    cp$partname <- needpart
 
     print(sprintf("Comp:%s need:%s",compname,needpart))
     tform <- xml_find_first(it, ".//*[local-name()='Transform']")
     if (length(tform) > 0) {
       nrot <- xml_find_first(it, ".//*[local-name()='Rotation']")
       rot <- matrix(as.numeric(str_split(xml_text(nrot), "\\s")[[1]]),3,3)
-      thisCompPart$rot <- rot
+      cp$rot <- rot
       ntrn <- xml_find_first(it,".//*[local-name()='Translation']")
       # no idea where this factor of 1000 comes from (mm -> meters?)
       # some STL brain damage no doubt
       trn <- 1000.0*as.numeric(str_split(xml_text(ntrn),"\\s")[[1]])
-      thisCompPart$trn <- trn
-      compList[[compname]] <- thisCompPart
+      cp$trn <- trn
+      compList[[compname]] <- cp
     }
   }
   return(compList)
 }
 
-getCompElement <- function(compList,need) {
-  #print(sprintf("getElement:%s",need))
-  for (p in compList) {
-    cname <- p$partname
-    #print(sprintf("   detecting:%s",cname))
-    if (str_detect(cname,need)) {
-      #print(sprintf("returning:%s",cname))
-      return(p)
-    }
-  }
-  #print(sprintf("returning NULL"))
-  return(NULL)
-}
-
-getAttElement <- function(partAttList,need) {
-  #print(sprintf("getElement:%s",need))
-  for (p in partAttList) {
-    pname <- p$partname
-    #print(sprintf("   detecting:%s",pname))
-    if (str_detect(pname,need)) {
-      #print(sprintf("returning:%s",pname))
-      return(p)
-    }
-  }
-  #print(sprintf("returning NULL"))
-  return(NULL)
-}
-
-grabMaterials <- function(stldir,xfname) {
+readMaterialsFromXml <- function(stldir,xfname) {
   partAttList <- list()
   xmlfile <- sprintf("%s/%s",stldir,xfname)
   doc <- read_xml(xmlfile)
@@ -428,9 +371,7 @@ grabMaterials <- function(stldir,xfname) {
   return(partAttList)
 }
 
-
-
-printComposition <- function(compList) {
+dumpCompList <- function(compList) {
   for (cp in compList) {
     print(sprintf("%s - %s",cp$compname,cp$partname))
     print(cp$rot)
@@ -438,14 +379,27 @@ printComposition <- function(compList) {
   }
 }
 
-compList <- grabComposition(stldir,"Crazyflie_assembly.xml")
-partAttList <- grabMaterials(stldir,"Crazyflie_assembly.xml")
-#printComposition(compList)
+# Start of actual program
+# =======================
+starttime <- Sys.time()
 
+stldir <- "../Crazyflie-CAD/STL"
 
+# Get the STL files with the vertices and read them in
+stlfiles <- list.files(stldir,"\\.STL$")
+partVertList <- list()
+for (fname in stlfiles) {
+  partVertList <- readVertsFromStl(stldir,fname,partVertList)
+}
 
-plotWholeThing(partAttList,partList,compList)
-#d <- read_xml("simple1.xml")
-#bstree <- xml_find_all(d, ".//bs:stmt") # Transaction Type
+# Now read the composistion file which has the part instances, 
+#  their transformations which position them, and their material properties
+# 
+compList <- readCompositionFromXml(stldir,"Crazyflie_assembly.xml")
+partAttList <- readMaterialsFromXml(stldir,"Crazyflie_assembly.xml")
+#dumpCompList(compList)
+
+plotWholeThing(partAttList,partVertList,compList)
+
 elap <- as.numeric((Sys.time() - starttime)[1], units = "secs")
-print(sprintf("Run took %.1f secs for %d verts - saved:%d", elap,nvtot,nvsaved))
+print(sprintf("Run took %.1f secs for %d verts - verts optimized away:%d", elap,nvtot,nvsaved))
