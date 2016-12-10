4265fc6170c91c68c6e33cc5eb4e7e982699a896
Mike Wise <mwise@microsoft.com>
2016-12-10 22:52:41 +0100

    fixed axes

--- readcf1.R
+++ readcf1.R
@@ -1,6 +1,4 @@
 library(rgl)
-library(stringr)
-library(xml2)
 library(hash)
 #
 #  Mike Wise - Nov/Dec 2016
@@ -60,16 +58,49 @@ readMesh <- function(fnameroot = "crazyflie") {
 }
 
 
-addAxes <- function(len = 1,t = c(0,0,0),r = NULL,tit = "") {
-  u <- c(0,1) * len
+addAxes <- function(len = 1,s = NULL,r = NULL,t = NULL,tit = "",charexp = 1) {
+
+  getHeadToTailPoints <- function(x,y,z) {
+    m <- matrix(c(x,y,z),2,3)
+    return(m)
+  }
+  u <- c(0,len)
   v <- c(0,0)
   w <- c(0,0)
-  lines3d(u + t[1],v + t[2],w + t[3],color = c("red"))
-  text3d(u + t[1],v + t[2],w + t[3],c("","X"),color = c("red"),cex = 1)
-  lines3d(w + t[1],u + t[2],v + t[3],color = c("green"))
-  text3d(w + t[1],u + t[2],v + t[3],c("","Y"),color = c("green"))
-  lines3d(v + t[1],w + t[2],u + t[3],color = c("blue"))
-  text3d(v + t[1],w + t[2],u + t[3],c("","Z"),color = c("blue"))
+
+  xax <- getHeadToTailPoints(u,v,w)
+  yax <- getHeadToTailPoints(w,u,v)
+  zax <- getHeadToTailPoints(v,w,u)
+
+  if (!is.null(s)) {
+    xax[1,] <- xax[1,] %*% s
+    xax[2,] <- xax[2,] %*% s
+    yax[1,] <- yax[1,] %*% s
+    yax[2,] <- yax[2,] %*% s
+    zax[1,] <- zax[1,] %*% s
+    zax[2,] <- zax[2,] %*% s
+  }
+  if (!is.null(r)) {
+    xax <- xax %*% r
+    yax <- yax %*% r
+    zax <- zax %*% r
+  }
+  if (!is.null(t)) {
+    xax[1,] <- xax[1,] + t
+    xax[2,] <- xax[2,] + t
+    yax[1,] <- yax[1,] + t
+    yax[2,] <- yax[2,] + t
+    zax[1,] <- zax[1,] + t
+    zax[2,] <- zax[2,] + t
+  }
+
+  lines3d( xax,color = c("red"))
+  lines3d( yax,color = c("green"))
+  lines3d( zax,color = c("blue"))
+
+  text3d(xax[2,],text="X",color = c("red"),cex = charexp)
+  text3d(yax[2,],text="Y",color = c("green"),cex = charexp)
+  text3d(zax[2,],text="Z",color = c("blue"),cex = charexp)
 
 }
 
